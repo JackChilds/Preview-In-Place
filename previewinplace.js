a6b39d42d74c69fbcadeddb560739bd4c343b3c0
JackChilds <35969199+JackChilds@users.noreply.github.com>
2021-10-29 13:45:04 +0100

    update

--- previewinplace.js
+++ previewinplace.js
@@ -6,23 +6,26 @@ class PreviewInPlace {
     init(block, options) {
         let opts = {
             previewGenerator: undefined, // required: the function callback that processes the data and returns the preview HTML. Note: return value is not sanitised in any way and is injected as HTML
-            defaultIsPreview: true, // optional: sets 
+            defaultIsPreview: true, // optional: sets default mode
             classPrefix: '', // optional: prefix that is added before class names such as 'edit'
             editValue: false, // optional: this._edit.value is used by default, unless a function reference is specified
-            loadDefaultCSS: true // optional: minimal css file that places the .edit and .preview elements on top of each other. Recommended to leave as true
+            loadDefaultCSS: true, // optional: minimal css file that places the .edit and .preview elements on top of each other. Recommended to leave as true
+            events: { // optional:
+                onPreview: undefined, // this function is called when the element is in preview mode
+                onEdit: undefined // this function is called when the element is in edit mode
+            }
         }
 
         for (const [key, value] of Object.entries(opts)) {
             options[key] = options[key] === undefined ? opts[key] : options[key];
         }
 
-        console.log(options);
-
         this._defaultIsPreview = options.defaultIsPreview;
         this._previewGeneratorCallback = options.previewGenerator;
         this._classPrefix = options.classPrefix;
         this._editValue = options.editValue;
         this._loadDefaultCSS = options.loadDefaultCSS;
+        this._events = options.events;
 
         this._defaultCSS = '.' + this._classPrefix + 'block{position:relative;display:grid}.' + this._classPrefix + 'block .' + this._classPrefix + 'edit,.' + this._classPrefix + 'block .' + this._classPrefix + 'preview{grid-column:1;grid-row:1}';
         this._injectDefaultCSS();
@@ -52,7 +55,6 @@ class PreviewInPlace {
             // check if css has already been injected by a previous class instance
             var styleTag = document.head.querySelector('[data-fdsjkekwwriuehiucnjkdseje]'); // the attribute that is added to the style tag containing the default css when it is created with no relevance to anything but an attribute that won't get in the way of the application
             if (styleTag == null) {
-                console.log("adding css")
                 var s = document.createElement('style');
                 s.textContent = this._defaultCSS;
                 s.setAttribute('data-fdsjkekwwriuehiucnjkdseje', '')
@@ -62,8 +64,13 @@ class PreviewInPlace {
         }
     }
 
-    _blockClicked(event) {
-        event.stopPropagation();
+    _blockClicked(event = undefined) {
+        if (event !== undefined) {
+            event.stopPropagation();
+        }
+        if (this._events.onEdit !== undefined) {
+            this._events.onEdit();
+        }
         this._block.classList.add(this._classPrefix + 'state-edit');
         this._block.classList.remove(this._classPrefix + 'state-preview');
         this._edit.style.display = 'block';
@@ -72,6 +79,9 @@ class PreviewInPlace {
         this._isInPreview = false;
     }
     _editBlurred() {
+        if (this._events.onPreview !== undefined) {
+            this._events.onPreview();
+        }
         this._block.classList.remove(this._classPrefix + 'state-edit');
         this._block.classList.add(this._classPrefix + 'state-preview');
         this._edit.style.display = 'none';
@@ -80,30 +90,43 @@ class PreviewInPlace {
         this._isInPreview = true;
     }
     _getEditValue() {
-        if (this._editValue === false) {
+        if (this._editValue === false || this._editValue === undefined) {
             return this._edit.value;
         } else {
             return this._editValue()
         }
     }
 
-    reset() {
-        // after the class has been reset, it must be initialised again throught the init() function
-        this._block.removeEventListener('click', this._blockClicked)
-        this._edit.removeEventListener('blur', this._editBlurred)
-        this._block.classList.remove(this._classPrefix + 'state-edit');
-        this._block.classList.remove(this._classPrefix + 'state-preview');
-        this._isInPreview = undefined;
-        this._edit.style.display = 'block'
-        this._preview.style.display = 'block'
-
-        this._block = undefined;
-        this._edit = undefined;
-        this._preview = undefined;
+    set mode(mode) {
+        if (mode.toLowerCase() === 'edit') {
+            this._blockClicked();
+        } else {
+            this._editBlurred();
+        }
     }
 
-    get editValue() {
+    get mode() {
+        if (this._isInPreview) {
+            return 'preview';
+        } else {
+            return 'edit';
+        }
+    }
+
+    toggleMode() {
+        if (this.mode == 'preview') {
+            this.mode = 'edit';
+        } else {
+            this.mode = 'preview';
+        }
+    }
+
+    get editVal() {
         return this._getEditValue()
+    }
+
+    get previewVal() {
+        return this._preview.innerHTML;
     }
 
     get el() {
